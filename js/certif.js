92cc842d8ad2089b032f56e029e7b1c27d42035d
jolan <jolan@jolan.home>
2025-02-04 11:45:17 +0100

    terminal affichage

--- js/certif.js
+++ js/certif.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     const projectContainer = document.querySelector('.container.mt-5 .row');
 
@@ -18,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
             modal.setAttribute('aria-labelledby', 'genericModalLabel');
             modal.setAttribute('aria-hidden', 'true');
             modal.innerHTML = `
-                <div class="modal-dialog" style="max-width: 50vw; max-height: 80vh;">
+                <div class="modal-dialog" style="max-width: 60%; max-height: 80vh;">
                     <div class="modal-content">
                         <div class="modal-header">
                             <h5 class="modal-title" id="genericModalLabel"></h5>
@@ -109,4 +108,64 @@ document.addEventListener('DOMContentLoaded', () => {
     script.onerror = () => {
         console.error('Erreur lors du chargement de PDF.js');
     };
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const terminalOutput = document.getElementById('output');
+
+    // Liste des lignes à afficher dans le terminal
+    const lines = [
+        { input: 'Wohami@kali:~$ echo "Mes certifications et diplomes"', output: 'Mes certifications et diplomes' },
+        { input: 'Wohami@kali:~$ ls', output: 'Projets  Contact  À propos' },
+        { input: 'Wohami@kali:~$ cat projet.txt', output: 'Mon projet actuel est une application web de cybersécurité.' },
+        { input: 'Wohami@kali:~$ date', output: '2025-02-04 14:35:47' },
+        { input: 'Wohami@kali:~$ exit', output: 'Déconnexion...' }
+    ];
+
+    let currentLine = 0;
+
+    function typeLine(line, index) {
+        let charIndex = 0;
+        const typingSpeed = 50; // Vitesse de frappe en ms
+
+        // Affichage de la ligne de commande
+        const inputLine = document.createElement('div');
+        inputLine.classList.add('input-line');
+        terminalOutput.appendChild(inputLine);
+        inputLine.innerHTML = line.input;
+
+       
+
+        function typeOutput() {
+            const outputLine = document.createElement('div');
+            outputLine.classList.add('output-line');
+            terminalOutput.appendChild(outputLine);
+
+            let outputCharIndex = 0;
+
+            // Fonction pour afficher le texte de sortie de manière "tapée"
+            function typeChar() {
+                outputLine.innerHTML += line.output[outputCharIndex];
+                outputCharIndex++;
+
+                if (outputCharIndex < line.output.length) {
+                    setTimeout(typeChar, typingSpeed);
+                } else {
+                    terminalOutput.innerHTML += '\n'; // Nouvelle ligne après chaque sortie
+                    currentLine++;
+                    if (currentLine < lines.length) {
+                        setTimeout(typeLine, 500, lines[currentLine], currentLine); // Attendre 500ms avant de commencer la prochaine ligne
+                    }
+                }
+            }
+
+            typeChar();
+        }
+
+        typeOutput();
+    }
+
+    // Lancer le processus d'affichage des lignes dans le terminal
+    typeLine(lines[currentLine], currentLine);
 });
---
+++ js/contact.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const terminalOutput = document.getElementById('output');
+
+    // Liste des lignes à afficher dans le terminal
+    const lines = [
+        { input: 'Wohami@kali:~$ echo "Mes certifications et diplomes"', output: 'Mes certifications et diplomes' },
+        { input: 'Wohami@kali:~$ ls', output: 'Projets  Contact  À propos' },
+        { input: 'Wohami@kali:~$ cat projet.txt', output: 'Mon projet actuel est une application web de cybersécurité.' },
+        { input: 'Wohami@kali:~$ date', output: '2025-02-04 14:35:47' },
+        { input: 'Wohami@kali:~$ exit', output: 'Déconnexion...' }
+    ];
+
+    let currentLine = 0;
+
+    function typeLine(line, index) {
+        let charIndex = 0;
+        const typingSpeed = 50; // Vitesse de frappe en ms
+
+        // Affichage de la ligne de commande
+        const inputLine = document.createElement('div');
+        inputLine.classList.add('input-line');
+        terminalOutput.appendChild(inputLine);
+        inputLine.innerHTML = line.input;
+
+       
+
+        function typeOutput() {
+            const outputLine = document.createElement('div');
+            outputLine.classList.add('output-line');
+            terminalOutput.appendChild(outputLine);
+
+            let outputCharIndex = 0;
+
+            // Fonction pour afficher le texte de sortie de manière "tapée"
+            function typeChar() {
+                outputLine.innerHTML += line.output[outputCharIndex];
+                outputCharIndex++;
+
+                if (outputCharIndex < line.output.length) {
+                    setTimeout(typeChar, typingSpeed);
+                } else {
+                    terminalOutput.innerHTML += '\n'; // Nouvelle ligne après chaque sortie
+                    currentLine++;
+                    if (currentLine < lines.length) {
+                        setTimeout(typeLine, 500, lines[currentLine], currentLine); // Attendre 500ms avant de commencer la prochaine ligne
+                    }
+                }
+            }
+
+            typeChar();
+        }
+
+        typeOutput();
+    }
+
+    // Lancer le processus d'affichage des lignes dans le terminal
+    typeLine(lines[currentLine], currentLine);
+});
